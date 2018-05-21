bc18496c30cfae14c09c56de081ddb1b11c185ea
u6325688@anu.edu.au <yangyang.xu1@anu.edu.au>
2018-05-21 10:25:38 +1000

    add

--- Code/DeepModel/Exp_RNN.py
+++ Code/DeepModel/Exp_RNN.py
@@ -9,104 +9,146 @@ import torch.optim as optim
 from torch.autograd import Variable
 import torch.nn.functional as F
 
+##-------------------------------------------------
+# DATA Preparation
+##-------------------------------------------------
+Normalisation_location = "../data_norm/"
+data = np.load(Normalisation_location + 'yn_question_datapoints.npy')
+np.random.shuffle(data)
+# chars = list(set(data))
+# data_size, vocab_size = len(data), len(chars)
+# print('data has %d characters, %d unique.' % (data_size, vocab_size))
+# char_to_ix = {ch: i for i, ch in enumerate(chars)}
+# ix_to_char = {i: ch for i, ch in enumerate(chars)}
 
-# data load dataset
-data = open('textData.txt', 'r').read() # should be simple plain text file
-chars = list(set(data))
-data_size, vocab_size = len(data), len(chars)
-print('data has %d characters, %d unique.' % (data_size, vocab_size))
-char_to_ix = { ch:i for i,ch in enumerate(chars) }
-ix_to_char = { i:ch for i,ch in enumerate(chars) }
+msk = np.random.rand(len(data)) < 0.8
+
+train_data = data[msk]
+test_data = data[~msk]
+n_attributes = train_data.shape[1] - 1
+
+train_input = train_data[:, :n_attributes]
+train_target = train_data[:, n_attributes]
+
+test_input = test_data[:, :n_attributes]
+test_target = test_data[:, n_attributes]
+
+X_train = Variable(torch.Tensor(train_input).float())
+Y_train = Variable(torch.Tensor(train_target).long())
+
+X_test = Variable(torch.Tensor(test_input).float())
+Y_test = Variable(torch.Tensor(test_target).long())
+
+##-------------------------------------------------
+# LSTM Hyperparameters
+##-------------------------------------------------
+INPUT_SIZE = 118  # 11 vectors
+TOTAL_SIZE = len(train_input)
+OUTPUT_SIZE = 2
+HIDDEN_SIZE =70  # size of hidden layer of neurons
+NUM_LAYER = 1  #
+NUM_STEP = 50  # number of steps to unroll the RNN for
+learning_rate = 0.009
+num_epochs = 85
 
 
-# hyperparameters
-hidden_size = 100 # size of hidden layer of neurons
-seq_length = 50 # number of steps to unroll the RNN for
-learning_rate = 1e-1
+##-------------------------------------------------
+# LSTM Models
+##-------------------------------------------------
+class LSTM_GFE(nn.Module):
+    def __init__(self):
+        super(LSTM_GFE, self).__init__()
+        self.lstm = nn.LSTM(
+            input_size=INPUT_SIZE,
+            hidden_size=HIDDEN_SIZE,
+            num_layers=NUM_LAYER,
+            batch_first=True,
+        )
+        self.FC = nn.Linear(HIDDEN_SIZE, OUTPUT_SIZE)
 
-class _charLSTM(nn.Module):
-   def __init__(self):
-      super(_charLSTM, self).__init__()
-      self.LSTM = nn.LSTM(len(chars), hidden_size, 1)
-      self.FC = nn.Linear(hidden_size, len(chars))
-
-   def forward(self, input, hprev, cprev, seq_len):
-      output, hc = self.LSTM(input, (hprev, cprev))
-      output = self.FC(output)
-      return output.view(seq_len, len(chars)), hc
+    def forward(self, input):
+        input = input.contiguous().view(input.data.shape[0], 1, input.data.shape[1])
+        print("Continue: "+str(input.shape))
+        output, hc = self.lstm(input, None)
+        output = self.FC(output[:, -1, :])
+        return output
+        # return output.view(TOTAL_SIZE), hc
 
 
-charLSTM = _charLSTM()
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(charLSTM.parameters(), lr=learning_rate)
+##-------------------------------------------------
+# TRAINING
+##-------------------------------------------------
+def TrainModel(Model, h, c, num_seq):
+    loss_func = torch.nn.CrossEntropyLoss()
+    optimiser = torch.optim.Adam(Model.parameters(), lr=learning_rate)
+
+    all_train_losses = []
+    all_test_losses = []
+    hc = None;
+
+    print(X_train.shape)
+    # exit(0)
+    for epoch in range(num_epochs):
+        Y_predict = Model(X_train)
+        loss = loss_func(Y_predict, Y_train)
+        all_train_losses.append(loss.data[0])
+        Model.zero_grad()
+        loss.backward()
+        optimiser.step()
+
+        Y_pred_test = Model(X_test)
+        loss_test = loss_func(Y_pred_test, Y_test)
+        all_test_losses.append(loss_test.data[0])
+
+    import matplotlib.pyplot as plt
+
+    plt.figure()
+    plt.plot(all_train_losses)
+    plt.plot(all_test_losses, color="green")
+    plt.show()
 
 
-def sample(h, c, seed_ix, n):
-  """
-  sample a sequence of outputs from the model
-  h and c are memory states,
-  seed_ix is seed letter for first time step,
-  n is number of outputs to sample
-  """
-  letter = torch.zeros(1, 1, len(chars))
-  letter.data[0][0][seed_ix] = 1
-  letters = []
-  """
-  sample letters one at a time so output at previous step 
-  becomes input for next step
-  """
-  for t in range(n):
-    y, hc = charLSTM(letter, h, c, 1)
-    p = F.softmax(y,1)
-    l = np.random.choice(range(vocab_size), p=p.data.numpy().ravel())
-    letters.append(l)
-    letter = torch.zeros(1,1, len(chars))
-    letter.data[0][0][l] = 1
-    h = hc[0]
-    c = hc[1]
-  return letters
+##-------------------------------------------------
+# MODEL Evaluation
+##-------------------------------------------------
+def EvaModel(Model):
+    confusion = torch.zeros(OUTPUT_SIZE, OUTPUT_SIZE)
+    Y_predict = Model(X_train)
+    _, predicted = torch.max(Y_predict, 1)
 
-p,n = 0,0
-smooth_loss = -np.log(1.0/vocab_size)# loss at iteration 0
-while True:
-  # prepare inputs (we're sweeping from left to right in steps seq_length long)
-  if p+seq_length+1 >= len(data) or n == 0:
-    hprev = Variable(torch.zeros(1, 1, hidden_size)) # reset RNN memory
-    cprev = Variable(torch.zeros(1, 1, hidden_size))
-    p = 0 # go from start of data
-  inputs = [char_to_ix[ch] for ch in data[p:p+seq_length]]
-  targets = [char_to_ix[ch] for ch in data[p+1:p+seq_length+1]]
-  targets = torch.from_numpy(np.array(targets))
-  targets = Variable(targets)
+    for i in range(train_data.shape[0]):
+        actual_class = Y_train.data[i]
+        predicted_class = predicted.data[i]
 
-  # sample from the model now and then
+        confusion[actual_class][predicted_class] += 1
+    print('')
+    print('Confusion matrix for training 1:')
+    print(confusion)
 
-  if n % 1000 == 0:
-    sample_ix = sample(hprev, cprev, inputs[0], 200)
-    txt = ''.join(ix_to_char[ix] for ix in sample_ix)
-    print('----\n %s \n----' % (txt, ))
+    Y_pred_test = Model(X_test)
+    _, predicted_test = torch.max(Y_pred_test, 1)
+    total_test = predicted_test.size(0)
+    correct_test = sum(predicted_test.data.numpy() == Y_test.data.numpy())
+    print('Testing Accuracy: %.4f %%' % (correct_test / total_test))
 
-  # forward seq_length characters through the net
-  input = torch.zeros(seq_length,1, len(chars))
-  for i,inp in enumerate(input):
-    inp[0][inputs[i]]=1
-  input = Variable(input)
-  #charLSTM.zero_grad()
-  optimizer.zero_grad()
-  output, hcprev = charLSTM(input, hprev, cprev, seq_length)
-  hprev = hcprev[0]
-  cprev = hcprev[1]
-  hprev.detach()
-  cprev.detach()
+    confusion_test = torch.zeros(OUTPUT_SIZE, OUTPUT_SIZE)
+    for i in range(test_data.shape[0]):
+        actual_class = Y_test.data[i]
+        predicted_class = predicted_test.data[i]
 
-  # get loss and gradients and adjust the weights
-  err = criterion(output, targets)
-  #err.backward()
-  optimizer.step()
+        confusion_test[actual_class][predicted_class] += 1
 
-  smooth_loss = smooth_loss * 0.999 + err.items() * 0.001
-  if n % 1000 == 0:
-    print('iter %d, loss: %f' % (n, smooth_loss)) # print progress
+    print('')
+    print('Confusion matrix for testing 1:')
+    print(confusion_test)
 
-  p += seq_length # move data pointer
-  n += 1 # iteration counter
+
+net = LSTM_GFE()
+print(net)
+TrainModel(net, 1, 1, 1)
+EvaModel(net)
+
+
+
+
+
--- Code/DeepModel/LSTM.py
+++ Code/DeepModel/LSTM.py
@@ -13,13 +13,13 @@ import torch.nn.functional as F
 # DATA Preparation
 ##-------------------------------------------------
 Normalisation_location = "../data_norm/"
-data = np.load(Normalisation_location+'yn_question_datapoints.npy')
+data = np.load(Normalisation_location + 'yn_question_datapoints.npy')
 np.random.shuffle(data)
-#chars = list(set(data))
-#data_size, vocab_size = len(data), len(chars)
-#print('data has %d characters, %d unique.' % (data_size, vocab_size))
-#char_to_ix = {ch: i for i, ch in enumerate(chars)}
-#ix_to_char = {i: ch for i, ch in enumerate(chars)}
+# chars = list(set(data))
+# data_size, vocab_size = len(data), len(chars)
+# print('data has %d characters, %d unique.' % (data_size, vocab_size))
+# char_to_ix = {ch: i for i, ch in enumerate(chars)}
+# ix_to_char = {i: ch for i, ch in enumerate(chars)}
 
 msk = np.random.rand(len(data)) < 0.8
 
@@ -42,13 +42,16 @@ Y_test = Variable(torch.Tensor(test_target).long())
 ##-------------------------------------------------
 # LSTM Hyperparameters
 ##-------------------------------------------------
-INPUT_SIZE = 118 #11 vectors
+INPUT_SIZE = 118  # 118 vectors
+TOTAL_SIZE = len(train_input)
 OUTPUT_SIZE = 2
-HIDDEN_SIZE = 100  # size of hidden layer of neurons
-NUM_LAYER = 1 #
-NUM_STEP = 50  # number of steps to unroll the RNN for
-learning_rate = 1e-1
+HIDDEN_SIZE = 10  # size of hidden layer of neurons
+NUM_LAYER = 1  #
+NUM_STEP = 5  # number of steps to unroll the RNN for
+learning_rate = 0.01
 num_epochs = 10
+
+
 ##-------------------------------------------------
 # LSTM Models
 ##-------------------------------------------------
@@ -57,43 +60,72 @@ class LSTM_GFE(nn.Module):
         super(LSTM_GFE, self).__init__()
         self.lstm = nn.LSTM(
             input_size=INPUT_SIZE,
-            hidden_size = HIDDEN_SIZE,
+            hidden_size=HIDDEN_SIZE,
             num_layers=NUM_LAYER,
             batch_first=True,
         )
         self.FC = nn.Linear(HIDDEN_SIZE, OUTPUT_SIZE)
 
-    def forward(self, input,hprev):
-        output, hc = self.lstm(input, hprev)
-        output = self.FC(output)
-        return output.view(INPUT_SIZE), hc
+    def forward(self, input,  hprev, cprev):
+        input = input.contiguous().view(input.data.shape[0], 1, input.data.shape[1])
+        print("Continue: "+str(input.shape))
+        output, hc = self.lstm(input, (hprev, cprev))
+        output = self.FC(output[:, -1, :])
+        #print(output.shape)
+        return output.view(output.shape[0],output.shape[1]), hc
+
+
 ##-------------------------------------------------
 # TRAINING
 ##-------------------------------------------------
-def TrainModel(Model, h, c, num_seq):
+def TrainModel(Model):
     loss_func = torch.nn.CrossEntropyLoss()
     optimiser = torch.optim.Adam(Model.parameters(), lr=learning_rate)
 
     all_train_losses = []
     all_test_losses = []
-    hc = None;
 
-    #ToDo: input Size????
-    
     print(X_train.shape)
-   # exit(0)
+    ct = 0
+    # exit(0)
+    out_size = NUM_STEP
     for epoch in range(num_epochs):
-        Y_predict, hprev = Model(X_train,hc)
-        hc = Variable(hprev.data) #Update hc
-        loss = loss_func(Y_predict, Y_train)
-        all_train_losses.append(loss.data[0])
-        Model.zero_grad()
-        loss.backward()
-        optimiser.step()
+        train_ct = 0
+        hprev = Variable(torch.zeros(1, out_size, HIDDEN_SIZE))  # reset RNN memory
+        cprev = Variable(torch.zeros(1, out_size, HIDDEN_SIZE))
+        ccc = 0
+        for entry in X_train:
+            #print(entry)
+            #print(entry.shape[0])
+            #print(entry.shape[1])
 
-        Y_pred_test = Model(X_test,hc)
-        loss_test = loss_func(Y_pred_test, Y_test)
-        all_test_losses.append(loss_test.data[0])
+            if train_ct%NUM_STEP == 0 and train_ct !=0:
+                Y_predict, hc = Model(X_train[train_ct-NUM_STEP:train_ct],hprev, cprev)
+                hprev = hc[0]
+                cprev = hc[1]
+                hprev.detach_()
+                cprev.detach_()
+                loss = loss_func(Y_predict, Y_train[train_ct-NUM_STEP:train_ct])
+                all_train_losses.append(loss.data[0])
+                optimiser.zero_grad()
+                loss.backward()
+                optimiser.step()
+                print("#" + str(train_ct))
+                ccc+=1
+                print(ccc)
+            train_ct+=1
+
+
+        if epoch % 2 == 0:
+            Y_pred_test, hc = Model(X_test,hprev, cprev)
+            #pred_y = torch.max(Y_pred_test, 1)[1].data.numpy().squeeze()
+            loss_test = loss_func(Y_pred_test, Y_test)
+            all_test_losses.append(loss_test.data[0])
+            #accuracy = sum(pred_y == Y_test) / float(Y_test.size)
+            print('Epoch: ', epoch, '| train loss: %.4f' % loss.data[0], '| test loss: %.4f' % loss_test.data[0])
+
+        ct+=1
+
 
     import matplotlib.pyplot as plt
 
@@ -101,6 +133,8 @@ def TrainModel(Model, h, c, num_seq):
     plt.plot(all_train_losses)
     plt.plot(all_test_losses, color="green")
     plt.show()
+
+
 ##-------------------------------------------------
 # MODEL Evaluation
 ##-------------------------------------------------
@@ -135,11 +169,13 @@ def EvaModel(Model):
     print('Confusion matrix for testing 1:')
     print(confusion_test)
 
-
+##-------------------------------------------------
+# Start
+##-------------------------------------------------
 
 net = LSTM_GFE()
 print(net)
-TrainModel(net,1,1,1)
+TrainModel(net)
 EvaModel(net)
 
 
